9582953d7b39efb970c9cc24c3825310afa2908c
jonas-henry <jonashenrique57@gmail.com>
2026-07-20 17:19:53 -0300

    configurei as rotas

---
+++ src/routes/router.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import MainLayout from '@components/layout/MainLayout/MainLayout';
+
+import Home from '@pages/Home/Home';
+import About from '@pages/About/About';
+import Services from '@pages/Services/Services';
+import Contact from '@pages/Contact/Contact';
+import Booking from '@pages/Booking/Booking';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'services',
+        element: <Services />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'booking',
+        element: <Booking />,
+      },
+    ],
+  },
+]);
+
+export default router;
