b0d33b7047c8ec56ecfa6e4a48e647463faf2b48
jonas-henry <jonashenrique57@gmail.com>
2026-07-21 16:27:11 -0300

    organizei estrutura main layout

--- src/components/layout/MainLayout/MainLayout.jsx
+++ src/components/layout/MainLayout/MainLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import styles from './MainLayout.module.css';
 
 function MainLayout() {
   return (
-    <>
-      <Outlet />
-    </>
+    <div className={styles.layout}>
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+    </div>
   );
 }
 
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import MainLayout from '@components/layout/MainLayout/MainLayout';
+import MainLayout from '@components/layout/MainLayout';
 
 import Home from '@pages/Home/Home';
 import About from '@pages/About/About';
